ca8792c9d7d28a88ee0d98d402064fb1e2c83785
mark hancock <mwhancock2@gmail.com>
2022-04-07 21:58:42 -0230

    Add round structure

--- script.js
+++ script.js
@@ -1,8 +1,39 @@
 
+
 function computerPlay(){
-  const computerChoice = ['Rock', 'Paper', 'Scissors'];
-  const i = Math.floor(Math.random() * computerChoice.length);
-  return computerChoice[i];
+  const options = ['rock', 'paper', 'scissors'];
+  let computerChoice = Math.floor(Math.random() * options.length);
+  return options[computerChoice];
 }
 
-alert(computerPlay());
+
+let computerSelection = computerPlay();
+let playerSelection = prompt('Choose: rock, paper, or scissors').toLowerCase();
+
+function playRound(x, y){
+if (x === 'rock' && y ==='paper'){
+  return 'You win! Paper beats rock!';
+} else if (x === 'paper' && y === 'rock'){
+  return 'You lose! Paper beats rock!';
+} else if (x === 'scissors' && y === "rock"){
+  return 'You win! Rock beats scissors!';
+} else if (x === 'rock' && y === 'scissors'){
+  return 'You lose! Rock beats scissors!';
+} else if (x === 'paper' && y === 'scissors'){
+  return'You win! Scissors beat paper!';
+} else if (x === 'scissors' && y === 'paper'){
+  return 'You lose! Scissors beat paper!';
+}else if (x === y){
+  return 'You tied! Try again!';
+}
+}
+
+function game(round){
+  let comp = 0;
+  let player = 0;
+  for (let i = 0; i < 3; i++){
+    console.log(round);
+  }
+}
+
+console.log(game(playRound()));
